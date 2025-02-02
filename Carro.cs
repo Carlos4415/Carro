06016d089d6ffe5ce6f65a23a63ec25a3dd178ed
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:34:44 -0300

    first commit

---
+++ Carro.cs
@@ -0,0 +1,38 @@
+﻿class Carro
+{
+    public string fabricante;
+    public string modelo;
+    public int ano;
+    public int quantidadePortas;
+    public int velocidade;
+
+    public void exibirInformacoes()
+    {
+        Console.WriteLine($"\nInformações do carro: {this.fabricante} {this.modelo}, {this.quantidadePortas} portas, {this.ano}");
+    }
+
+    public void acelerar()
+    {
+        Console.WriteLine("\nAcelerando...");
+
+        if (velocidade < 100)
+        {
+            velocidade = velocidade + 5;
+        }
+    }
+
+    public void frear()
+    {
+        Console.WriteLine("\nFreando...");
+
+        if (velocidade > 0)
+        {
+            velocidade = velocidade - 5;
+        }
+    }
+
+    public void buzinar()
+    {
+        Console.WriteLine("\nBi Bi");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Este projeto implementa uma classe que modela um carro.  
+// A classe possui os seguintes atributos e métodos:  
+// - Atributos: podem incluir modelo, marca, ano, cor e velocidade atual.  
+// - Métodos:  
+//   - Acelerar: aumenta a velocidade do carro.  
+//   - Frear: reduz a velocidade do carro.  
+//   - Buzinar: emite um som característico de buzina.  
+// O objetivo é simular o comportamento básico de um carro, permitindo interações simples como acelerar, frear e buzinar.  
+Carro carro = new Carro();
+carro.fabricante = "Honda";
+carro.modelo = "Civic";
+carro.ano = 2020;
+carro.quantidadePortas = 4;
+
+carro.exibirInformacoes();
+carro.acelerar();
+Console.WriteLine($"\nVelocidade: {carro.velocidade}");
+carro.frear();
+Console.WriteLine($"\nVelocidade: {carro.velocidade}");
+carro.buzinar();
